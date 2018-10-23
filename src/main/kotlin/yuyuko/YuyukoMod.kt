dff41c74a762627664b219d6cd7c285d295085b8
Gogo <gogo81745.moe@gmail.com>
2018-10-23 14:52:17 +0800

    Version 0.2.5

--- src/main/kotlin/yuyuko/YuyukoMod.kt
+++ src/main/kotlin/yuyuko/YuyukoMod.kt
@@ -41,7 +41,7 @@ class YuyukoMod : PostInitializeSubscriber, EditCardsSubscriber, EditCharactersS
     companion object {
         const val MODNAME = "YuyukoMod"
         const val AUTHOR = "Gogo, RuoRan"
-        const val DESCRIPTION = "YuyukoMod v0.2.4"
+        const val DESCRIPTION = "YuyukoMod v0.2.5"
 
         @JvmStatic
         fun initialize() {
