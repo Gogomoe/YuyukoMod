234ab82854375b5ff0a5b85c1f735012148d57d5
Gogo <gogo81745.moe@gmail.com>
2018-11-18 14:25:00 +0800

    Fix a bug in LosePotionSlotAction that crash the game.

--- src/main/kotlin/yuyuko/actions/LosePotionSlotAction.kt
+++ src/main/kotlin/yuyuko/actions/LosePotionSlotAction.kt
@@ -3,7 +3,9 @@ package yuyuko.actions
 import com.megacrit.cardcrawl.actions.AbstractGameAction
 import com.megacrit.cardcrawl.core.Settings
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.potions.AbstractPotion
 import com.megacrit.cardcrawl.potions.PotionSlot
+import java.util.*
 
 class LosePotionSlotAction : AbstractGameAction() {
 
@@ -18,15 +20,19 @@ class LosePotionSlotAction : AbstractGameAction() {
         val potions = player.potions
         val index = potions.indexOfFirst { it is PotionSlot }
 
-        player.obtainPotion(index, potions[index + 1].makeCopy())
+        println(index)
+        player.obtainPotion(index, copyNextPotion(potions, index))
         for (i in (index + 1) until player.potionSlots) {
             player.removePotion(potions[i])
-            player.obtainPotion(i, potions[i + 1].makeCopy())
+            player.obtainPotion(i, copyNextPotion(potions, i))
         }
         potions.removeAt(player.potionSlots)
 
         player.adjustPotionPositions()
         this.isDone = true
     }
+
+    private fun copyNextPotion(potions: ArrayList<AbstractPotion>, index: Int) =
+            potions[index + 1].makeCopy() ?: PotionSlot(index)
 
 }
